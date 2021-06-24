4caa7b928637d6637a0434cf3926dcad0353d052
Winnie-Wong-Binlo <winnie@binlo.io>
2021-06-24 18:05:24 +0800

    add screen and navigation files

---
+++ src/screens/UserScreen/index.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function UserScreen() {
+    return (
+        <div>
+
+        </div>
+    )
+}
