5df02686f3d723713ed78042dbc474150d4851ce
Winnie-Wong-Binlo <winnie@binlo.io>
2021-06-24 11:51:59 +0800

    initial commit

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>test</Text>
+      <Text>test</Text>
       <StatusBar style="auto" />
     </View>
   );
